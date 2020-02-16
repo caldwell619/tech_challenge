3ada13f9066a6c3a1aebcfa425ff8e6b4ffa180a
caldwell619 <caldwell8814@gmail.com>
2020-02-15 20:52:48 -0600

    pruning user context

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import MainRouter from "./router/MainRouter";
-import Loading from "./components/Loading";
-import persistedUser, { UserContext } from "./context/UserContext";
-import { LoadingContext } from "./context/LoadingContext";
+import Router from "router/Router";
+import Loading from "components/Loading";
+import { LoadingContext } from "context/LoadingContext";
 import "./css/App.css";
 
 const persistedMode = JSON.parse(window.localStorage.getItem("persistedMode"));
 
 export default () => {
-	const [themeMode, setThemeMode] = useState(persistedMode || "light");
+	const [themeMode, setThemeMode] = useState(persistedMode || "dark");
 	const theme = createMuiTheme({
 		palette: {
 			type: themeMode, // Switching the dark mode on is a single property value change.
@@ -18,19 +17,16 @@ export default () => {
 	});
 
 	const [isLoading, toggleLoading] = useState(false);
-	const [user, updateUser] = useState(persistedUser || null);
 
 	return (
 		<ThemeProvider theme={theme}>
 			<div className="App">
-				<UserContext.Provider value={{ user, updateUser }}>
-					<LoadingContext.Provider value={{ isLoading, toggleLoading }}>
-						<Loading />
-						<div className="content-window">
-              <MainRouter user={user} currentTheme={themeMode} setThemeMode={setThemeMode} />
-						</div>
-					</LoadingContext.Provider>
-				</UserContext.Provider>
+				<LoadingContext.Provider value={{ isLoading, toggleLoading }}>
+					<Loading />
+					<div className="content-window">
+						<Router currentTheme={themeMode} setThemeMode={setThemeMode} />
+					</div>
+				</LoadingContext.Provider>
 			</div>
 		</ThemeProvider >
 	);
