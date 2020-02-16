ca498bb78df76f893e3efa8190b3ed9416f1d055
caldwell619 <caldwell8814@gmail.com>
2020-02-15 20:39:39 -0600

    setting up initial structure

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import MainRouter from "./router/MainRouter";
+import Loading from "./components/Loading";
+import persistedUser, { UserContext } from "./context/UserContext";
+import { LoadingContext } from "./context/LoadingContext";
+import "./css/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const persistedMode = JSON.parse(window.localStorage.getItem("persistedMode"));
 
-export default App;
+export default () => {
+	const [themeMode, setThemeMode] = useState(persistedMode || "light");
+	const theme = createMuiTheme({
+		palette: {
+			type: themeMode, // Switching the dark mode on is a single property value change.
+			},
+	});
+
+	const [isLoading, toggleLoading] = useState(false);
+	const [user, updateUser] = useState(persistedUser || null);
+
+	return (
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<UserContext.Provider value={{ user, updateUser }}>
+					<LoadingContext.Provider value={{ isLoading, toggleLoading }}>
+						<Loading />
+						<div className="content-window">
+              <MainRouter user={user} currentTheme={themeMode} setThemeMode={setThemeMode} />
+						</div>
+					</LoadingContext.Provider>
+				</UserContext.Provider>
+			</div>
+		</ThemeProvider >
+	);
+};
