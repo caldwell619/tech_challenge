e3127107dc0b66daf28065943097c87d2fbfd682
caldwell619 <caldwell8814@gmail.com>
2020-02-15 20:55:00 -0600

    adding loading spinner component

---
+++ ui/src/components/util/Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Spinner from "@material-ui/core/CircularProgress";
+import { LoadingContext } from "../context/LoadingContext";
+
+const Loading = () => (
+	<LoadingContext.Consumer>
+		{({ isLoading }) => {
+			return isLoading ? (
+				<div className="spinner-container">
+					<Spinner className="spinner" />
+				</div>
+			) : (
+					<div />
+				);
+		}}
+	</LoadingContext.Consumer>
+);
+
+Loading.propTypes = {
+	isLoading: PropTypes.bool
+};
+
+export default Loading;
