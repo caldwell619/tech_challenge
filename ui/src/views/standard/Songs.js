8cabc8ad7d20e6b7508eb06ba51498cbb546587a
caldwell619 <caldwell8814@gmail.com>
2020-02-16 16:06:11 -0600

    implementing sort

--- ui/src/views/standard/Songs.js
+++ ui/src/views/standard/Songs.js
@@ -1,32 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+
 import Category from 'components/songs/Category'
 import Search from 'components/songs/Search'
 import IndividualSong from 'components/songs/IndividualSong'
 import LocalLoading from 'components/util/LocalLoading'
-import { categoriesOfMetrics } from 'data/songsMetaData'
+import Pagination from 'components/songs/Pagination'
+
 import { filterSongs } from 'util/logicHelper'
+import quickSort from 'util/quickSort'
+
+import { categoriesOfMetrics } from 'data/songsMetaData'
 
 import client from 'client'
 
 // local songs - no need for api
-// import songs from 'data/songs'
+import songs from 'data/temp-songs'
 
 // any state change on this page is very slow due to all the songs being shown. If pagination could be implemented, that would help a great deal.
 const Songs = () => {
   const [availableSongs, setAvailableSongs] = useState([])
   const [baseSongs, setBaseSongs] = useState([])
+  const [sortDirection, setSortDirection] = useState('')
   const [isLoading, setIsLoading] = useState(false)
-  const [currentSortCategory, setCurrentSortCategory] = useState('')
+  const [currentSortCategory, setCurrentSortCategory] = useState({})
 
   useEffect(() => {
     const fetchSongs = async () => {
       try {
         setIsLoading(true)
-        const { data } = await client.get('/songs')
-        setAvailableSongs(data)
-        setBaseSongs(data)
+        // const { data } = await client.get('/songs')
+        setAvailableSongs(songs)
+        setBaseSongs(songs)
         setIsLoading(false)
       } catch(error){
         setIsLoading(false)
@@ -52,8 +58,10 @@ const Songs = () => {
     setIsLoading(false)
   }
   
-  const handleSortClick = category => {
-    setCurrentSortCategory(category)
+  const handleSortClick = ({ programmaticCategory, categoryText }) => {
+    const sortedSongsByCategory = quickSort(availableSongs, 0, availableSongs.length -1, programmaticCategory)
+    setAvailableSongs(sortedSongsByCategory)
+    setCurrentSortCategory({programmaticCategory, categoryText})
   }
 
   return (
@@ -70,6 +78,14 @@ const Songs = () => {
           <Search handleSearch={handleSearch}/>
         </Grid>
       </Grid>
+      <Grid container justify='flex-start' alignItems='center'>
+        <Grid item md={1}>
+          <Typography variant='h5' >Sorted By:</Typography>
+        </Grid>
+        <Grid item md={2}>
+          <Typography variant='h5' style={{marginLeft: '5%'}}>{currentSortCategory.categoryText}</Typography>
+        </Grid>
+      </Grid>
       <div className="songs-container">
         <div className='table'>
           <div className='table-row'>
@@ -77,7 +93,7 @@ const Songs = () => {
               <Category 
                 category={category} 
                 handleSortClick={handleSortClick} 
-                isCurrentlySorted={category.programmaticCategory === currentSortCategory}
+                isCurrentlySorted={category.programmaticCategory === currentSortCategory.programmaticCategory}
                 key={category.programmaticCategory}
               />
             ))}
@@ -85,6 +101,7 @@ const Songs = () => {
             {availableSongs.map((song, index) => <IndividualSong song={song} index={index} key={`song-data-${Math.random()}-${index}`}/>)}
         </div>
       </div>
+      <Pagination/>
     </div>
   );
 };
