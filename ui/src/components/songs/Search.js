ddd25d1763db12f0f51e708bc3551c360e9b1f5b
caldwell619 <caldwell8814@gmail.com>
2020-02-18 17:16:03 -0600

    removing button from search

--- ui/src/components/songs/Search.js
+++ ui/src/components/songs/Search.js
@@ -1,23 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
-import SearchIcon from '@material-ui/icons/Search'
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1)
-  },
-  rightIcon: {
-    marginLeft: theme.spacing(1)
-  }
-}));
 
 const Search = ({ handleSearch }) => {
-  const classes = useStyles();
-
   const [ searchTerm, setSearchTerm ] = useState('')
   const handleSubmit = event => {
     event.preventDefault();
@@ -27,7 +13,7 @@ const Search = ({ handleSearch }) => {
   }
 
   return (
-    <Grid container component='form' justify='space-between' alignItems='center' noValidate>
+    <Grid container component='form' justify='flex-end' noValidate>
       <Grid item className="search-term-cont" md={7}>
         <TextField
           fullWidth
@@ -36,17 +22,6 @@ const Search = ({ handleSearch }) => {
           value={searchTerm}
           onChange={handleSubmit}
         />
-      </Grid>
-      <Grid item className="search-term-cta" md={4}>
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          size="medium"
-          onClick={handleSubmit}
-        >
-          Search <SearchIcon className={classes.rightIcon} />
-        </Button>
       </Grid>
     </Grid>
   );
