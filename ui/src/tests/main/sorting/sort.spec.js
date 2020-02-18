9e558cbe4dc2883ee33fa74b67822eec9f437f79
caldwell619 <caldwell8814@gmail.com>
2020-02-17 18:25:24 -0600

    seeding correct values propely

--- ui/src/tests/main/sorting/sort.spec.js
+++ ui/src/tests/main/sorting/sort.spec.js
@@ -1,6 +1,7 @@
 import sort from "util/sortPrimitives"
-import unsortedSongs from "../lib/unsortedSongs.json"
-import correctlySortedSongs from "../lib/correctlySortedSongs.json"
+import unsortedSongs from "tests/lib/unsortedSongs.json"
+import correctlySortedAscendingSongs from "tests/lib/correctlySortedAscending.json"
+import correctlySortedDescendingSongs from "tests/lib/correctlySortedDescending.json"
 
 const songProperties = Object.keys(unsortedSongs[0]);
 
@@ -14,21 +15,21 @@ describe("Primitive sorting", () => {
 
 describe('Songs sorted in ascending order are correctly sorted', () => {
   songProperties.forEach(property => {
-    test(`Property ${property} is sorted correctly`, () => {
+    test(`Property "${property}" is sorted correctly`, () => {
       const sortedSongs = sort(unsortedSongs, true, property)
       const stringifiedSongs = JSON.stringify(sortedSongs)
-      const correctlySortedSongsStringified = JSON.stringify(correctlySortedSongs[property])
+      const correctlySortedSongsStringified = JSON.stringify(correctlySortedAscendingSongs[property])
       expect(stringifiedSongs).toBe(correctlySortedSongsStringified)
     })
   })
 })
 
-describe('Songs sorted in ascending order are correctly sorted', () => {
+describe('Songs sorted in descending order are correctly sorted', () => {
   songProperties.forEach(property => {
-    test(`Property ${property} is sorted correctly`, () => {
-      const sortedSongs = sort(unsortedSongs, true, property)
+    test(`Property "${property}" is sorted correctly`, () => {
+      const sortedSongs = sort(unsortedSongs, false, property)
       const stringifiedSongs = JSON.stringify(sortedSongs)
-      const correctlySortedSongsStringified = JSON.stringify(correctlySortedSongs[property])
+      const correctlySortedSongsStringified = JSON.stringify(correctlySortedDescendingSongs[property])
       expect(stringifiedSongs).toBe(correctlySortedSongsStringified)
     })
   })
